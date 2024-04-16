ba86cfeb986ce02e08cd316cb10d7a2191d6a4bc
pythonLoader <shashata@mit.edu>
2024-04-16 12:52:43 -0400

    Fixed discrepancy for the changes

--- smartem/offline/train_mb_error_detector/make_errnet_training.py
+++ smartem/offline/train_mb_error_detector/make_errnet_training.py
@@ -4,27 +4,13 @@ parser = argparse.ArgumentParser(description='Generates an ERRNET training datas
 parser.add_argument('--model_weights', help='model weights of EM2MB network')
 parser.add_argument('--in_dataset_h5', help='dataset with short dwell time images and ground truth masks')
 parser.add_argument('--out_dataset_h5', help='path of dataset to generate')
-parser.add_argument('-dwt',type=int,default=50, help='dwell time images to be used')
+parser.add_argument('--base_dwt',type=int,default=50, help='dwell time images to be used')
 parser.add_argument('--slowest_dwt',type=int,default=1200, help='slowest dwell time images to be used')
-parser.add_argument('--make_EM2Err',type=bool,default=False, help='make EM images and error mask dataset')
+parser.add_argument('--make_EM2Err',type=bool,default=False, help='Make dataset where x=<EM image> and y=<membrane prediction discrepancy from slowest dwell time image>')
 args=parser.parse_args()
-import sys, os
 
-try:
-    # from smartem.online.models import UNet
-    from smartem.offline.train_mb_error_detector.NNtools import UNet
-    from smartem.segmentation import segmenter
-except:
-
-    sys.path.append('/home/ssawmya-local/FM_work/SmartEM/')
-    sys.path.append('/home/ssawmya-local/FM_work/SmartEM/smartem/segmentation')
-    # from smartem.online.models import UNet
-    from smartem.offline.train_mb_error_detector.NNtools import UNet
-    from smartem.segmentation import segmenter
-
-
-
-
+from smartem.offline.train_mb_error_detector.NNtools import UNet
+from smartem.segmentation import segmenter
 from tools import get_error_map, get_error_GT
 import torch
 import h5py
@@ -35,7 +21,7 @@ device="cuda" if torch.cuda.is_available() else "cpu"
 in_dataset_h5=args.in_dataset_h5
 out_dataset_h5=args.out_dataset_h5
 model_weights=args.model_weights
-dwt=args.dwt
+base_dwt=args.base_dwt
 slowest_dwt = args.slowest_dwt
 EM2err = args.make_EM2Err
 
@@ -61,7 +47,8 @@ with h5py.File(in_dataset_h5, 'r') as h5:
 
     
     for reg in tqdm(regs, desc="generating membrane predictions..."):
-        im,mask=h5[reg+"/"+str(dwt)+"/im"],h5[reg+"/"+str(dwt)+"/mask"]
+        # im,mask=h5[reg+"/"+str(base_dwt)+"/im"],h5[reg+"/"+str(base_dwt)+"/mask"]
+        im = h5[reg+"/"+str(base_dwt)+"/im"]
         im = im[:]
         # Make predictions
         # mask=Iseg.preprocess(mask)
@@ -87,17 +74,17 @@ with h5py.File(in_dataset_h5, 'r') as h5:
         else:
             data_to_save[reg] = (mb_probs, emap)
         out_regs.append(reg)
-        # break
+        break
 # Save data
 with h5py.File(out_dataset_h5, 'a') as h5:
     for reg in data_to_save.keys():
         mb_probs, labels = data_to_save[reg]
-        g = h5.create_group(f"{reg}/{dwt}")
+        g = h5.create_group(f"{reg}/{base_dwt}")
         g.create_dataset("im", data = mb_probs)
         g.create_dataset("mask", data = labels)
 
     
-    dwts = [dwt]
+    dwts = [base_dwt]
     out_regs.sort()
     h5.attrs["dwts"] = dwts
     h5.attrs["regs"] = out_regs
