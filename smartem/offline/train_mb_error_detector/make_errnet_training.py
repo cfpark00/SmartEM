90bb6a7cc31b7be766a94a4a31b62368f257725c
pythonLoader <shashata@mit.edu>
2024-04-16 11:41:14 -0400

    Updated the script to compute correct error maps using membrane probabilities

--- smartem/offline/train_mb_error_detector/make_errnet_training.py
+++ smartem/offline/train_mb_error_detector/make_errnet_training.py
@@ -1,15 +1,34 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Generates an ERRNET training dataset given a membrane predictor, and short dwell time images with ground truth membrane masks.')
-parser.add_argument('model_weights', help='model weights of EM2MB network')
-parser.add_argument('in_dataset_h5', help='dataset with short dwell time images and ground truth masks')
-parser.add_argument('out_dataset_h5', help='path of dataset to generate')
+parser.add_argument('--model_weights', help='model weights of EM2MB network')
+parser.add_argument('--in_dataset_h5', help='dataset with short dwell time images and ground truth masks')
+parser.add_argument('--out_dataset_h5', help='path of dataset to generate')
 parser.add_argument('-dwt',type=int,default=50, help='dwell time images to be used')
+parser.add_argument('--slowest_dwt',type=int,default=1200, help='slowest dwell time images to be used')
+parser.add_argument('--make_EM2Err',type=bool,default=False, help='make EM images and error mask dataset')
 args=parser.parse_args()
 
-from FM_work.SmartEM.smartem.segmentation import UNet
+
+import sys, os
+
+import importlib
+try:
+    # from smartem.online.models import UNet
+    from smartem.offline.train_mb_error_detector.NNtools import UNet
+    from smartem.segmentation import segmenter
+except:
+
+    sys.path.append('/home/ssawmya-local/FM_work/SmartEM/')
+    sys.path.append('/home/ssawmya-local/FM_work/SmartEM/smartem/segmentation')
+    # from smartem.online.models import UNet
+    from smartem.offline.train_mb_error_detector.NNtools import UNet
+    from smartem.segmentation import segmenter
+
+
+
+
 from tools import get_error_map, get_error_GT
-from smartem.segmentation import segmenter
 import torch
 import h5py
 from tqdm import tqdm
@@ -20,6 +39,9 @@ in_dataset_h5=args.in_dataset_h5
 out_dataset_h5=args.out_dataset_h5
 model_weights=args.model_weights
 dwt=args.dwt
+slowest_dwt = args.slowest_dwt
+EM2err = args.make_EM2Err
+
 
 # Make segmenter object which will perform prediction
 net = UNet.UNet(n_channels=1,n_classes=2)
@@ -28,34 +50,47 @@ Iseg.set_model(model_class=net)
 
 # Make predictions and compute errors
 with h5py.File(in_dataset_h5, 'r') as h5:
+
+
     regs=h5.attrs["regs"]
     data_to_save = {}
     out_regs = []
+
+    hdt_im, _ = h5[regs[0]+"/"+str(slowest_dwt)+"/im"], h5[regs[0]+"/"+str(slowest_dwt)+"/mask"]
+    _, hdt_mb_probs = Iseg.get_membranes(hdt_im, get_probs=True)
+    # print(np.unique(hdt_mb_probs))
+    # make hdt_mb_probs uint8
+    hdt_mb_probs = (hdt_mb_probs*255).astype(np.uint8)
+
+    
     for reg in tqdm(regs, desc="generating membrane predictions..."):
         im,mask=h5[reg+"/"+str(dwt)+"/im"],h5[reg+"/"+str(dwt)+"/mask"]
-
-        # print(f"image shape: {im.shape}, {im.dtype} {np.amin(im)}-{np.amax(im)} w/median {np.median(im)}, sum {np.sum(im)}")
-        # print(f"mask shape: {mask.shape}, {mask.dtype} {np.amin(mask)}-{np.amax(mask)} w/median {np.median(mask)}, sum {np.sum(mask)}")
-
+        im = im[:]
         # Make predictions
-        mask=Iseg.preprocess(mask)
-        mask = np.squeeze(mask)
+        # mask=Iseg.preprocess(mask)
+        # mask = np.squeeze(mask)
         _, mb_probs = Iseg.get_membranes(im, get_probs=True)
-        mask = (mask*255).astype(np.uint8)
+        # mask = (mask*255).astype(np.uint8)
         mb_probs = (mb_probs*255).astype(np.uint8)
         #print(f"predicted probs shape: {mb_probs.shape}, {mb_probs.dtype} {np.amin(mb_probs)}-{np.amax(mb_probs)} w/median {np.median(mb_probs)}, sum {np.sum(mask)}")
         #print(f"mask shape: {mask.shape}, {mask.dtype} {np.amin(mask)}-{np.amax(mask)} w/median {np.median(mask)}, sum {np.sum(mask)}")
 
         # Generate error mask
-        labels = get_error_GT(mb_probs, mask)
-        labels = labels.astype(np.uint8)*255
+
+        emap = get_error_GT(mb_probs, hdt_mb_probs)
+        emap = (emap*255).astype(np.uint8)
 
         # print(f"MB predictions shape: {mb_probs.shape}, {mb_probs.dtype} {np.amin(mb_probs)}-{np.amax(mb_probs)} w/median {np.median(mb_probs)}, sum {np.sum(mb_probs)}")
-        # print(f"error labels shape: {labels.shape}, {labels.dtype} {np.amin(labels)}-{np.amax(labels)} w/median {np.median(labels)}, sum {np.sum(labels)}")
+        # print(f"error labels shape: {emap.shape}, {emap.dtype} {np.amin(emap)}-{np.amax(emap)} w/median {np.median(emap)}, sum {np.sum(emap)}")
 
         # Organize data for saving later
-        data_to_save[reg] = (mb_probs, labels)
+        if EM2err:
+
+            data_to_save[reg] = (im, emap)
+        else:
+            data_to_save[reg] = (mb_probs, emap)
         out_regs.append(reg)
+        # break
 
 # Save data
 with h5py.File(out_dataset_h5, 'a') as h5:
