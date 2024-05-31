Fixed the ocassional softmax issue

smartem/segmentation/segmenter.py
@@ -72,7 +72,13 @@ class Segmenter:
             output = self.model(img).cpu()
             # binarize the output based on the threshold of 0.5
             if (output >= 0).all() and (output <= 1).all():
-                mask = output > 0.5
+                # output = torch.softmax(output, dim=1)
+                # check if sum across dimension 1 is 1
+                if (output.sum(dim=1) == 1).all():
+                    mask = output > 0.5
+                else:
+                    output = torch.softmax(output, dim=1)
+                    mask = output > 0.5
             else:
                 output = torch.softmax(output, dim=1)
                 mask = output > 0.5
@@ -83,7 +89,7 @@ class Segmenter:
         if not get_probs:
             return mask
         else:
-            output = output.squeeze().numpy()[1]
+            # output = output.squeeze().numpy()[1]
             return mask, output
 
     def get_labels(self, img):

smartem/segmentation/tests/test_segmenter.py
@@ -56,12 +56,13 @@ def model_files(tmp_path):
 
 def test_segmentation_output(model_files):
     em2mb_path, error_path = model_files
+    # em2mb_path =  "/home/ssawmya-local/FM_work/SmartEM/smartem/segmentation/unet_50_2.81e-02.pth"
     segmenter = Segmenter(model_path=em2mb_path)
     segmenter.set_model(UNet.UNet(1, 2))
 
     # Create random test image going from size 256x256 to 2048x2048 by going double each time
     test_images = []
-    for i in range(7, 11):
+    for i in range(7, 12):
         test_image = np.random.rand(2**i, 2**i)
         test_image = (test_image * 255).astype(np.uint8)
         # Get the output probabilities
@@ -72,14 +73,23 @@ def test_segmentation_output(model_files):
             probs <= 1
         ).all(), f"Probabilities are not between 0 and 1 for image size {2**i}x{2**i}"
         probs_sum = probs.sum(axis=1)
+        # print(probs_sum.shape)
+        # print(probs_sum)
+        # if probs_sum is not all ones, print the image and the probabilities
+        if not np.allclose(probs_sum, 1, atol=1e-6):
+            print(
+                f"Probabilities do not sum to 1 across the channel dimension for image size {2**i}x{2**i}"
+            )
+            print(test_image)
+            print(probs[0])
         assert np.allclose(
             probs_sum, 1, atol=1e-6
         ), f"Probabilities do not sum to 1 across the channel dimension for image size {2**i}x{2**i}"
 
         # Check for NaNs or Infs
-        assert not np.isnan(probs).any(), "Probabilities contain NaN values"
-        assert not np.isinf(probs).any(), "Probabilities contain Inf values"
+        assert not torch.isnan(probs).any(), "Probabilities contain NaN values"
+        assert not torch.isinf(probs).any(), "Probabilities contain Inf values"
 
         # Check for numerical stability
-        assert np.all(probs > 1e-8), "Probabilities contain very small values"
-        assert np.all(probs < 1 - 1e-8), "Probabilities contain very large values"
+        assert torch.all(probs > 1e-8), "Probabilities contain very small values"
+        assert torch.all(probs < 1 - 1e-8), "Probabilities contain very large values"