8c73c1c8fca0101a6fd03d9b82ac5ff14058b038
pythonLoader <shashata@mit.edu>
2024-06-06 17:58:52 -0400

    Fixed the older tests and restructured the segmentation output tests

--- smartem/segmentation/tests/test_segmenter.py
+++ smartem/segmentation/tests/test_segmenter.py
@@ -3,38 +3,8 @@ import torch
 from skimage import measure
 import smartem
 from smartem.segmentation.segmenter import Segmenter
-from unittest.mock import MagicMock
 from smartem.offline.train_mb_error_detector.NNtools import UNet
 import numpy as np
-
-
-def test_default_initialization():
-    segmenter = Segmenter()
-    expected_device = "cuda" if torch.cuda.is_available() else "cpu"
-    assert str(segmenter.device) == expected_device
-    assert segmenter.model is None
-    assert segmenter.model_path is None
-    assert segmenter.segmenter_function is None
-    assert segmenter.labels is None
-
-
-def test_custom_initialization():
-    model_path = (
-        "/home/ssawmya-local/FM_work/SmartEM/smartem/segmentation/unet_50_2.81e-02.pth"
-    )
-    segmenter_function = measure.label
-    segmenter = Segmenter(model_path=model_path, segmenter_function=segmenter_function)
-    assert segmenter.model_path == model_path
-    assert segmenter.segmenter_function == segmenter_function
-
-
-def test_model_loading():
-    mock_model_class = MagicMock()
-    mock_model_class.to.return_value = mock_model_class
-    model_path = (
-        "/home/ssawmya-local/FM_work/SmartEM/smartem/segmentation/unet_50_2.81e-02.pth"
-    )
-    segmenter = Segmenter(model_path=model_path)
 
 
 @pytest.fixture
@@ -54,17 +24,46 @@ def model_files(tmp_path):
     yield str(em2mb_path), str(error_path)
 
 
+def test_default_initialization():
+    segmenter = Segmenter()
+    expected_device = "cuda" if torch.cuda.is_available() else "cpu"
+    assert str(segmenter.device) == expected_device
+    assert segmenter.model is None
+    assert segmenter.model_path is None
+    assert segmenter.segmenter_function is None
+    assert segmenter.labels is None
+
+
+def test_custom_initialization(model_files):
+    em2mb_path, error_path = model_files
+    segmenter_function = measure.label
+    segmenter = Segmenter(model_path=em2mb_path, segmenter_function=segmenter_function)
+    assert segmenter.model_path == em2mb_path
+    assert segmenter.segmenter_function == segmenter_function
+
+
+def test_model_loading(model_files):
+    model_class = UNet.UNet(1, 2)
+    em2mb_path, error_path = model_files
+    segmenter = Segmenter(model_path=em2mb_path)
+    segmenter.set_model(model_class)
+    assert segmenter.model is not None
+
+
 def test_segmentation_output(model_files):
     em2mb_path, _ = model_files
     segmenter = Segmenter(model_path=em2mb_path)
     segmenter.set_model(UNet.UNet(1, 2))
-    # Create random test image going from size 256x256 to 2048x2048 by going double each time
-    test_images = []
+    # Create random test images going from size 256x256 to 2048x2048
     for i in range(7, 12):
         test_image = np.random.rand(2**i, 2**i)
         test_image = (test_image * 255).astype(np.uint8)
         # Get the output probabilities
         mask, probs = segmenter.get_membranes(test_image, get_probs=True)
+
+        # Check for NaNs or Infs
+        assert not torch.isnan(probs).any(), "Probabilities contain NaN values"
+        assert not torch.isinf(probs).any(), "Probabilities contain Inf values"
 
         # Check if probabilities are between 0 and 1
         assert (probs >= 0).all() and (
@@ -72,17 +71,7 @@ def test_segmentation_output(model_files):
         ).all(), f"Probabilities are not between 0 and 1 for image size {2**i}x{2**i}"
         probs_sum = probs.sum(axis=1)
 
-        # if probs_sum is not all ones, print the image and the probabilities
-        if not np.allclose(probs_sum, 1, atol=1e-6):
-            print(
-                f"Probabilities do not sum to 1 across the channel dimension for image size {2**i}x{2**i}"
-            )
-            print(test_image)
-            print(probs[0])
-        # Check for NaNs or Infs
-        assert not torch.isnan(probs).any(), "Probabilities contain NaN values"
-        assert not torch.isinf(probs).any(), "Probabilities contain Inf values"
-
-        # Check for numerical stability
-        assert torch.all(probs > 1e-8), "Probabilities contain very small values"
-        assert torch.all(probs < 1 - 1e-8), "Probabilities contain very large values"
+        probs_sum = probs.sum(axis=1)
+        assert np.allclose(
+            probs_sum, 1, atol=1e-6
+        ), f"Probabilities do not sum to 1 across the channel dimension for image size {2**i}x{2**i}"
